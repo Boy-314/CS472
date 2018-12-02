1637681c23c064eb6a1b47501d58c8df4831e259
Boy-314 <boy.314.yee.willie@gmail.com>
2018-12-01 22:14:09 -0500

    finished programming assignment 4

--- programming assignments/programming4/programming4.cc
+++ programming assignments/programming4/programming4.cc
@@ -13,6 +13,8 @@
 #include <vector>
 using namespace std;
 
+int N;
+
 // tree struct
 struct Tree
 {
@@ -146,6 +148,133 @@ pair<lexicon_entry,int> prefix_to_lexicon_entry(vector<string> S, int K)
 	ret.second = K;
 	return ret;
 }
+
+// main cyk-parse algorithm
+vector<vector<vector<Tree>>> CYKPARSE(vector<grammar_entry> grammar,vector<lexicon_entry> lexicon, string user_input, vector<string> non_terms)
+{	
+	// UNUSED: convert user input into all lower case
+	// transform(userInput.begin(), userInput.end(), userInput.begin(), ::tolower);
+	
+	// split user input
+	vector<string> sentence = split(user_input);
+	
+	N = sentence.size();
+	
+	// setup P chart
+	Tree placeholder = {-1,-1,-1,"XXXXXXX",NULL,NULL,0};
+	vector<vector<vector<Tree>>> P(lexicon.size(),vector<vector<Tree>>(N,vector<Tree>(N,placeholder)));
+		
+	for(int i = 0; i < N; i++)
+	{
+		string word = sentence[i];
+		
+		// for each rule of form "POS --> Word [prob]" in the grammar
+		for(int j = 0; j < lexicon.size(); j++)
+		{
+			// find index of POS
+			int POS = 0;
+			while(non_terms[POS] != lexicon[j].part_of_speech)
+			{
+				POS++;
+			}
+			if(word == lexicon[j].word)
+			{
+				Tree tree = {POS,i,i,word,NULL,NULL,lexicon[j].probability};
+				P[POS][i][i] = tree;
+			}
+		}
+	}
+	
+	// length = length of the phrase
+	for(int length = 2; length < N + 1; length++)
+	{
+		// i = start of the phrase
+		for(int i = 0; i < N + 1 - length; i++)
+		{
+			// j = end of the phrase
+			int j = i + length - 1;
+			for(int NonTerm = 0; NonTerm < non_terms.size(); NonTerm++)
+			{
+				Tree tree = {NonTerm,i,j,"XXXXXXX",NULL,NULL,0.0};
+				P[NonTerm][i][j] = tree;
+				
+				// k = end of first subphrase
+				for(int k = i; k < j; k++)
+				{
+					// for each rule of form "M -> Y,Z [prob]" in the grammar
+					for(int grammarRule = 0; grammarRule < grammar.size(); grammarRule++)
+					{
+						// find indices of M,Y,Z
+						int M = 0;
+						while(non_terms[M] != grammar[grammarRule].root)
+						{
+							M++;
+						}
+						int Y = 0;
+						while(non_terms[Y] != grammar[grammarRule].left_child)
+						{
+							Y++;
+						}
+						int Z = 0;
+						while(non_terms[Z] != grammar[grammarRule].right_child)
+						{
+							Z++;
+						}
+						double newProb = P[Y][i][k].prob * P[Z][k + 1][j].prob * grammar[grammarRule].probability;
+						if(newProb > P[M][i][j].prob)
+						{
+							P[M][i][j].left = &(P[Y][i][k]);
+							P[M][i][j].right = &(P[Z][k + 1][j]);
+							P[M][i][j].prob = newProb;
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	return P;
+}
+
+// stringstream that will hold each sentence as a whole (from the printTree pre-order traversal)
+stringstream buffer;
+
+// print tree method
+void printTree(Tree tree, vector<string> non_terms, string flag)
+{
+	// if not a grammatically correct sentence
+	if(tree.prob == 0)
+	{
+		buffer << "This sentence cannot be parsed. Wrong";
+		if(flag == "yes")
+		{
+			cout << "This sentence cannot be parsed. Wrong" << endl;
+		}
+		return;
+	}
+	
+	// otherwise output tree with pre-order search
+	buffer << non_terms[tree.phrase] << " ";
+	if(flag == "yes")
+	{
+		cout << non_terms[tree.phrase] << " ";
+	}
+	
+	if(tree.word != "XXXXXXX")
+	{
+		buffer << tree.word << " ";
+		if(flag == "yes")
+		{
+			cout << tree.word << " ";
+		}
+	}
+	else
+	{
+		printTree(*(tree.left),non_terms,flag);
+		printTree(*(tree.right),non_terms,flag);
+	}
+}
+
 
 int main(int argc, char * argv[])
 {
@@ -294,7 +423,7 @@ int main(int argc, char * argv[])
 		}
 		
 		// output a listing of the grammatical rules (including lexical rules), with associated probability
-		cout << "Grammar\n";
+		cout << "\nGrammar\n";
 		vector<grammar_entry> used;
 		bool dup = false;
 		for(auto i : grammar)
@@ -311,17 +440,136 @@ int main(int argc, char * argv[])
 			if(!dup)
 			{
 				cout << i.root.substr(1,i.root.length()) << " -> " << i.left_child.substr(1,i.left_child.length()) << " " << i.right_child.substr(1,i.right_child.length()) << " [" << i.probability << "]\n";
-			}
-			if(!dup)
-			{
 				used.push_back(i);
 			}
 			dup = false;
 		}
 		cout << endl;
+		// reset temporary variables
+		vector<lexicon_entry> temp;
+		dup = false;
 		cout << "Lexicon\n";
+		for(auto i : lexicon)
+		{
+			for(auto j : temp)
+			{
+				if(i.part_of_speech == j.part_of_speech && i.word == j.word)
+				{
+					dup = true;
+					break;
+				}
+			}
+			if(!dup)
+			{
+				cout << i.part_of_speech.substr(1,i.part_of_speech.length()) << " -> " << i.word << " [" << i.probability << "]\n";
+				temp.push_back(i);
+			}
+			dup = false;
+		}
+		
+		// create non_terms vector
+		vector<string> non_terms;
+		vector<string> exists;
+		dup = false;
+		for(auto i : grammar)
+		{
+			for(auto j : exists)
+			{
+				if(i.root == j)
+				{
+					dup = true;
+					break;
+				}
+			}	
+			if(!dup)
+			{
+				non_terms.push_back(i.root);
+				exists.push_back(i.root);
+			}
+			dup = false;
+		}
+		dup = false;
+		exists.clear();
+		for(auto i : lexicon)
+		{
+			for(auto j : exists)
+			{
+				if(i.part_of_speech == j)
+				{
+					dup = true;
+					break;
+				}
+			}
+			if(!dup)
+			{
+				non_terms.push_back(i.part_of_speech);
+				exists.push_back(i.part_of_speech);
+			}
+			dup = false;
+		}
+		cout << endl;
+		
+		// create user input from test_input
+		vector<string> sentences;
+		for(auto j : test_input)
+		{
+			vector<string> temp = split(j);
+			string sentence = "";
+			for(auto k : temp)
+			{
+				if(k[0] != '*' && k[0] != '+')
+				{
+					sentence += k + " ";
+				}
+			}
+			sentence = sentence.substr(0,sentence.length() - 1);
+			sentences.push_back(sentence);
+		}
+		// for(auto j : sentences)
+		// {
+			// cout << j << endl;
+		// }
 		
 		// run cyk parse
+		double Y = 0.0;
+		if(flag == "yes")
+		{
+			cout << "Parses\n";
+		}
+		for(int i = 0; i < sentences.size(); i++)
+		{
+			vector<vector<vector<Tree>>> P = CYKPARSE(grammar,lexicon,sentences[i],non_terms);
+			
+			// print the tree if flag is yes
+			printTree(P[0][0][N - 1],non_terms,flag);
+			// cout << endl << buffer.str() << endl << test_input[i] << endl;
+			string output = buffer.str().substr(0,buffer.str().length() - 1);
+			// cout << output.length() << ", " << test_input[i].length() << endl;
+			if(output.compare(test_input[i]) == 0)
+			{
+				if(flag == "yes")
+				{
+					cout << "Right\n";
+				}
+				Y++;
+			}
+			else if(buffer.str() == "This sentence cannot be parsed. Wrong")
+			{
+			}
+			else
+			{
+				if(flag == "yes")
+				{
+					cout << "Wrong\n";
+				}
+			}
+			buffer.str("");
+		}
+		
+		// output probabilities
+		double X = lines;
+		cout << "\nThe parser was tested on " << X << " sentences. It got " << Y << " correct, for an accuracy of " << Y/X << ".\n";
 	}
+	
 	return 0;
 }
---
+++ programming assignments/programming4/readme.md
@@ -0,0 +1,6 @@
+# Instructions For Running
+
+1. Compile by running `g++ -std=c++11 programming4.cc -o programming4`
+2. Run with `./programming4.exe X Y`
+3. Where X is the number of lines of training set and test set, and Y is either yes or no (case sensitive) for detailed parse output
+4. Note: CYK Parse is EXTREMELY sensitive to white space. Please check that EVERY SENTENCE has NO EMPTY SPACE at the end for accurate results.
